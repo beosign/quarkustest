28f0c6168f11436236ae2f1d5e6e6ca8934449e1
Florian Dahlmanns <florian.dahlmanns@googlemail.com>
2021-05-26 09:46:22 +0200

    disable native tests that do not work yet

--- src/test/java/de/beosign/quarkustest/NativeGreetingResourceIT.java
+++ src/test/java/de/beosign/quarkustest/NativeGreetingResourceIT.java
@@ -1,7 +1,7 @@
 package de.beosign.quarkustest;
 
 //@NativeImageTest => cannot be used with @Inject
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativeGreetingResourceIT {// extends GreetingResourceTest {
 
     // Execute the same tests but in native mode.
 }
